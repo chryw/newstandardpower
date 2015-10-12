ebf46d45aa66371fff32f09a3d5cfa39346374a6
Cherry Wang <cherryregister@gmail.com>
2015-10-12 19:34:08 +0000

    revised contact php with real recipient address. aligned address blocks.

--- contact.php
+++ contact.php
@@ -2,10 +2,11 @@
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
-$recipient = "cherryregister@gmail.com";
+$recipient = "contact@newstandardpower.com";
 $subject = "From New Standard Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!" . " -" . "<a href='index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+//echo "Thank You!" . " -" . "<a href='index.html' style='text-decoration:none;color:#0099dd;'> Return Home</a>";
+header('Location: http://newstandardpower.com');
 
 ?>
